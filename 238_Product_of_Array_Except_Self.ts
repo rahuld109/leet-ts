5551ba1af89193e4a97d905963651ea9e96ad2f0
Rahul Dogra <rahuldogra1998@gmail.com>
2022-05-28 05:42:13 +0530

    238. Product of Array Except Self

---
+++ 238_Product_of_Array_Except_Self.ts
@@ -0,0 +1,17 @@
+function productExceptSelf(nums: number[]): number[] {
+  let result: number[] = [],
+    prefix: number = 1,
+    postfix: number = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= postfix;
+    postfix *= nums[i];
+  }
+
+  return result;
+}
