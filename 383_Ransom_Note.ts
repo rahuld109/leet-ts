76e46369974399220dd6392fb992ce99f810a53e
Rahul Dogra <rahuldogra1998@gmail.com>
2022-07-20 00:05:48 +0530

    Update 383_Ransom_Note.ts

--- 383_Ransom_Note.ts
+++ 383_Ransom_Note.ts
@@ -1,17 +1,17 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-  let m = new Map();
+  let m = new Map<string, number>();
 
   if (ransomNote === null || magazine === null || magazine.length < ransomNote.length) return false;
 
   for (let char of magazine) {
-    m.set(char, m.get(char) ? m.get(char) + 1 : 1);
+    m.set(char, m.get(char) ? m.get(char)! + 1 : 1);
   }
 
   for (let char of ransomNote) {
     if (!m.has(char) || m.get(char) === 0) {
       return false;
     } else {
-      m.set(char, m.get(char) ? m.get(char) - 1 : 0);
+      m.set(char, m.get(char) ? m.get(char)! - 1 : 0);
     }
   }
 
