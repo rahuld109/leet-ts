bee853ce283da15a4fb77a4e66d4767708f889ed
Rahul Dogra <rahuldogra1998@gmail.com>
2022-07-19 23:59:56 +0530

    383_Ransom_Note

---
+++ 383_Ransom_Note.ts
@@ -0,0 +1,19 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  let m = new Map();
+
+  if (ransomNote === null || magazine === null || magazine.length < ransomNote.length) return false;
+
+  for (let char of magazine) {
+    m.set(char, m.get(char) ? m.get(char) + 1 : 1);
+  }
+
+  for (let char of ransomNote) {
+    if (!m.has(char) || m.get(char) === 0) {
+      return false;
+    } else {
+      m.set(char, m.get(char) ? m.get(char) - 1 : 0);
+    }
+  }
+
+  return true;
+}
