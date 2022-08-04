b0902fb66d316d67d26f7e2f46a66f44b33ba9c4
Rahul Dogra <rahuldogra1998@gmail.com>
2022-08-05 00:21:29 +0530

    Update 42_Trapping_Rain_Water.ts

--- 42_Trapping_Rain_Water.ts
+++ 42_Trapping_Rain_Water.ts
@@ -1,4 +1,4 @@
-// Brute Force 
+// Brute Force - O(n^2)
 
 function trap(height: number[]): number {
     let totalWater = 0;
@@ -19,3 +19,21 @@ function trap(height: number[]): number {
 };
 
 
+// Optimal Solution - O(n)
+function trap(height: number[]): number {
+    let left = 0, right = height.length - 1, maxLeft = 0, maxRight = 0, total = 0;
+    while(left < right){
+        if(height[left] <= height[right]){
+            if(height[left] >= maxLeft){
+                maxLeft = height[left] 
+            }
+            total += maxLeft - height[left++];
+        }else{
+            if(height[right] >= maxRight){
+                maxRight = height[right]
+            }
+            total += maxRight - height[right--];
+        }
+    }
+    return total
+};
